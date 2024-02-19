6eed8922af04674d49d3b0f6025b2cc3711e25be
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-02-19 12:07:41 +0000

    updates to the sim

--- Code/Biped_controller/__init__.py
+++ Code/Biped_controller/__init__.py
@@ -1,8 +1,12 @@
-
+import time
 class Biped:
     def __init__(self,engine,filepath,position=[0, 0, 1]):
         self.p=engine
         self.robot_id = self.p.loadURDF(filepath, position)  # Load URDF at position [0, 0, 0]
+        for _ in range(100):  # Run the simulation for 1000 steps (adjust as needed)
+            self.p.stepSimulation()
+            time.sleep(1./240.)
+        self.orientation=[-7.403932178333501e-18, -7.822274398480377e-17, 2.2034421452109355e-18]
     def positions(self):
         num_joints = self.p.getNumJoints(self.robot_id)
 
@@ -37,3 +41,7 @@ class Biped:
         orientation_euler = self.p.getEulerFromQuaternion(orientation)
         
         return orientation_euler
+    def get_position(self):
+        position, orientation = self.p.getBasePositionAndOrientation(self.robot_id)
+        return position
+        
