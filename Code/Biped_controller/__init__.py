112a69dbb84e9a03c744e925c40e170614ca2fc5
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-02-19 10:45:16 +0000

    Create __init__.py

---
+++ Code/Biped_controller/__init__.py
@@ -0,0 +1,39 @@
+
+class Biped:
+    def __init__(self,engine,filepath,position=[0, 0, 1]):
+        self.p=engine
+        self.robot_id = self.p.loadURDF(filepath, position)  # Load URDF at position [0, 0, 0]
+    def positions(self):
+        num_joints = self.p.getNumJoints(self.robot_id)
+
+        angular_positions = []
+        for i in range(num_joints):
+            joint_info = self.p.getJointInfo(self.robot_id, i)
+            joint_type = joint_info[2]
+            if joint_type == self.p.JOINT_REVOLUTE:
+                # For revolute joints, retrieve the angular position
+                joint_state = self.p.getJointState(self.robot_id, i)
+                angular_position = joint_state[0]
+                angular_positions.append(angular_position)
+        
+        return angular_positions
+    def set_positions(self,positions):
+        num_joints = self.p.getNumJoints(self.robot_id)
+
+        if len(positions) != num_joints:
+            print("Error: Number of angular positions does not match the number of joints.")
+            return
+
+        for i in range(num_joints):
+            joint_info = self.p.getJointInfo(self.robot_id, i)
+            joint_type = joint_info[2]
+            if joint_type == self.p.JOINT_REVOLUTE:
+                # For revolute joints, set the angular position
+                self.p.setJointMotorControl2(self.robot_id, i, self.p.POSITION_CONTROL, targetPosition=positions[i])
+    def get_orientation(self):
+        position, orientation = self.p.getBasePositionAndOrientation(self.robot_id)
+
+        # Convert quaternion to euler angles
+        orientation_euler = self.p.getEulerFromQuaternion(orientation)
+        
+        return orientation_euler
