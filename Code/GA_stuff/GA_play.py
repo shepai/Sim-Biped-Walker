ba6d99542355a370cccb3ba55312fe1f6ee05ecf
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-07-23 17:39:58 +0100

    Update GA_play.py

--- Code/GA_stuff/GA_play.py
+++ Code/GA_stuff/GA_play.py
@@ -5,7 +5,7 @@ if __name__=="__main__":
     sys.path.insert(1,"/its/home/drs25/Documents/GitHub/Sim-Biped-Walker/Code/")
     sys.path.insert(1,"C:/Users/dexte/Documents/GitHub/Sim-Biped-Walker/Code")
 datapath="/its/home/drs25/Documents/GitHub/Sim-Biped-Walker/"
-datapath="C:/Users/dexte/Documents/GitHub/Sim-Biped-Walker/"
+#datapath="C:/Users/dexte/Documents/GitHub/Sim-Biped-Walker/"
 
 from Biped_controller.pattern import sinBot
 from Biped_controller import environment, get_total_mass
@@ -14,18 +14,33 @@ import pickle
 from genetic_algorithms import *
 
 
+error=False
+top_geno=None
+top=-np.inf
+i=1
+while not error:
+    try:
+        file="models/genotypes_dt0.01_Microbial_F3_"+str(i)+"_friction0.5.pkl"
 
-file="models/genotypes_dt0.1_Hillclimber_F1_0_friction0.5.pkl"
+        with open(datapath + file, "rb") as f:
+            model = pickle.load(f)
 
-with open(datapath + file, "rb") as f:
-    model = pickle.load(f)
+        fitnesses=np.load(datapath +file.replace("genotypes","fitnesses").replace(".pkl",".npy"))
+        if np.max(fitnesses)>top:
+            top=np.max(fitnesses)
+            top_geno=model[np.argmax(fitnesses)]
+    except FileNotFoundError:
+        print(i)
+        error=True
+    i+=1
+print(top)
 
-fitnesses=np.load(file.replace("genotypes","fitnesses").replace(".pkl",".npy"))
-top=np.argmax(fitnesses)
 #print("@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n\n\n",fitnesses)
-env=environment(1,300,friction=0.5,filename="C:/Users/dexte/Documents/GitHub/Sim-Biped-Walker/assets/filerecord.mp4")
+env=environment(1,300,friction=0.5,filename=datapath+"/assets/filerecord.mp4")
 #print(model)
 env.reset()
 print("\n\n\n",get_total_mass(env.robot_id))
-env.runTrial(model[top],100,delay=1)
+env.runTrial(top_geno,1000,delay=0)
 env.close()
+
+print(list(top_geno.geno))
